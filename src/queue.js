f68c4ff9126aa18b9b6df8adc075b23e57b17238
Artur Tarasau <tarasov.a.web@gmail.com>
2023-04-15 13:35:40 +0300

    complite queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor(){
+    this.queue = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue( value ) {
+    this.queue = addNode(this.queue, value);
+
+    function addNode(node, value) {
+      if (!node) {
+        return new ListNode(value);
+      } else {
+        node.next = addNode(node.next, value);
+        return node;
+      }
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let headNode = this.queue.value;
+    this.queue = this.queue.next
+    return headNode
   }
 }
 
